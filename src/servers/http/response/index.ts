5567c123994ba8ebd68fc0162d31bc8976f40bbf
vimulatus <vasu.mit2501@gmail.com>
2024-10-15 18:50:44 +0530

    add basic http response
    support setting http status code in statusline

---
+++ src/servers/http/response/index.ts
@@ -0,0 +1,10 @@
+export class HttpResponse {
+  private statusline = "HTTP/1.1 ${status} ${message}";
+  constructor() { }
+
+  status(code: number) {
+    this.statusline.replace("${status}", code.toString());
+
+    return this;
+  }
+}
